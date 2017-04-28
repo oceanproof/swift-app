778860a721fe63cfa15559335d6b01c3e72334c1
Jacek Sojka <jaceksojka@p200300c12bd1ed371ce91408c05d14c0.dip0.t-ipconnect.de>
2017-04-28 13:30:53 +0200

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Jacek Sojka on 28.04.17.
+//  Copyright © 2017 Jacek Sojka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
